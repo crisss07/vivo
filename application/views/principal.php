8116d0510d3832e37e56b25539df6422f0023a43
crisss07 <rodri07crisss@gmail.com>
2019-10-10 17:13:20 -0400

    primer formulario de postulacion

--- application/views/principal.php
+++ application/views/principal.php
@@ -1,1 +1,135 @@
-hola
+<!-- begin::Body -->
+			<div class="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body">
+
+				<!-- BEGIN: Left Aside -->
+				
+
+					<!-- BEGIN: Aside Menu -->
+					
+
+					<!-- END: Aside Menu -->
+
+				<!-- END: Left Aside -->
+				<div class="m-grid__item m-grid__item--fluid m-wrapper">
+
+					<!-- BEGIN: Subheader -->
+					
+
+					<!-- END: Subheader -->
+					<div class="m-content">
+						<div class="row">
+							<div class="col-md-6">
+
+								<!--begin::Portlet-->
+								<div class="m-portlet m-portlet--tab">
+									<div class="m-portlet__head">
+										<div class="m-portlet__head-caption">
+											<div class="m-portlet__head-title">
+												<span class="m-portlet__head-icon m--hide">
+													<i class="la la-gear"></i>
+												</span>
+												<h3 class="m-portlet__head-text">
+													AEV - REGISTRO DE POSTULANTES
+												</h3>
+											</div>
+										</div>
+									</div>
+
+									<!--begin::Form-->
+									<form class="m-form m-form--fit m-form--label-align-right">
+										<div class="m-portlet__body">
+											<div class="form-group m-form__group">
+												<label for="exampleSelect1">¿A cu&aacute;l proyecto desea postular?</label>
+												<select class="form-control m-input" id="exampleSelect1">
+													<option>ELIJA UNA OPCION</option>
+													<option>WIPHALA(La Paz - El Alto - Villa Mercedario)</option>
+													<option>PAPA FRANCISCO (Santa Cruz)</option>
+													
+												</select>
+											</div>
+											<div class="form-group m-form__group row">
+												<div class="col-lg-4">
+													<label class="">N&uacute;mero de Carnet Identidad:</label>
+													<input type="text" class="form-control m-input" required>
+													<span class="m-form__help">Por favor ingrese su C.I.</span>
+												</div>
+											</div>
+											<div class="form-group m-form__group row">
+												<div class="col-lg-4">
+													<label class="">Nombres:</label>
+													<input type="text" class="form-control m-input">
+												</div>
+												<div class="col-lg-4">
+													<label class="">Apellido Paterno:</label>
+													<div class="m-input-icon m-input-icon--right">
+														<input type="text" class="form-control m-input">
+														<span class="m-input-icon__icon m-input-icon__icon--right"></span>
+													</div>
+												</div>
+												<div class="col-lg-4">
+													<label class="">Apellido Materno:</label>
+													<div class="m-input-icon m-input-icon--right">
+														<input type="text" class="form-control m-input">
+														<span class="m-input-icon__icon m-input-icon__icon--right"></span>
+													</div>
+												</div>
+											</div>
+											<div class="form-group m-form__group row">
+												<div class="col-lg-4">
+													<label class="">Domicilio:</label>
+													<input type="text" class="form-control m-input" required>
+													<span class="m-form__help">Por favor ingrese su domicilio</span>
+												</div>
+												<div class="col-lg-4">
+													<label class="">N&uacute;mero de Celular:</label>
+													<div class="m-input-icon m-input-icon--right">
+														<input type="text" class="form-control m-input" required>
+														<span class="m-input-icon__icon m-input-icon__icon--right"></span>
+													</div>
+													<span class="m-form__help">Por favor ingrese su # de Celular</span>
+												</div>
+												<div class="col-lg-4">
+													<label class="">Email:</label>
+													<div class="m-input-icon m-input-icon--right">
+														<input type="text" class="form-control m-input" required>
+														<span class="m-input-icon__icon m-input-icon__icon--right"></span>
+													</div>
+													<span class="m-form__help">Por favor ingrese su Correo Electr&oacute;nico</span>
+												</div>
+											</div>
+										</div>
+										<div class="m-portlet__foot m-portlet__foot--fit">
+											<div class="m-form__actions">
+												<button type="reset" class="btn btn-primary">Guardar</button>
+												<button type="reset" class="btn btn-secondary">Cancelar</button>
+											</div>
+										</div>
+									</form>
+
+									<!--end::Form-->
+								</div>
+
+								<!--end::Portlet-->
+
+								<!--begin::Portlet-->
+								
+
+								<!--end::Portlet-->
+
+								<!--begin::Portlet-->
+								
+
+								<!--end::Portlet-->
+
+								<!--begin::Portlet-->
+								
+
+								<!--end::Portlet-->
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- end:: Body -->
