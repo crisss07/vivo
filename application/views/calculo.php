2b77298edc73f8927dd73a2e093857913771399f
rodrigosecko <rodrigosecko@gmail.com>
2019-10-10 19:01:46 -0400

    formulario  de calculo

---
+++ application/views/calculo.php
@@ -0,0 +1,147 @@
+<!-- begin::Body -->
+			<div class="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body">
+
+				<!-- BEGIN: Left Aside -->
+				<button class="m-aside-left-close  m-aside-left-close--skin-dark " id="m_aside_left_close_btn"><i class="la la-close"></i></button>
+				<div id="m_aside_left" class="m-grid__item	m-aside-left  m-aside-left--skin-white ">
+
+					<!-- BEGIN: Aside Menu -->
+					
+
+					<!-- END: Aside Menu -->
+				</div>
+
+				<!-- END: Left Aside -->
+				<div class="m-grid__item m-grid__item--fluid m-wrapper">
+
+					<!-- BEGIN: Subheader -->
+					
+
+					<!-- END: Subheader -->
+					<div class="m-content">
+						<div class="row">
+							<div class="col-md-8">
+
+								
+
+								<!--begin::Portlet-->
+								<div class="m-portlet m-portlet--tab">
+									<div class="m-portlet__head">
+										<div class="m-portlet__head-caption">
+											<div class="m-portlet__head-title">
+												<span class="m-portlet__head-icon m--hide">
+													<i class="la la-gear"></i>
+												</span>
+												<h3 class="m-portlet__head-text">
+													CALCULA TU CREDITO
+												</h3>
+											</div>
+										</div>
+									</div>
+
+									<!--begin::Form-->
+									<form class="m-form m-form--fit m-form--label-align-right">
+										<div class="m-portlet__body">
+											
+											<div class="form-group m-form__group row">
+												<label for="example-text-input" class="col-7 col-form-label">1. ¿A cuánto alcanza tus ingresos liquidos mensuales? (Bs.)</label>
+												<div class="col-5">
+													<input class="form-control m-input" type="number" value="" id="example-text-input">
+												</div>
+											</div>
+											<div class="form-group m-form__group row">
+												<label for="example-search-input" class="col-7 col-form-label">2. ¿A cuanto asciende tus ingresos adicionales o de tu conyuge (si corresponde)? (Bs.)</label>
+												<div class="col-5">
+													<input class="form-control m-input" type="number" value="How do I shoot web" id="example-search-input">
+												</div>
+											</div>
+											<div class="form-group m-form__group row">
+												<label for="example-email-input" class="col-7 col-form-label">3. ¿Tienes préstamos en otras entidades financieras?</label>
+												<div class="col-5">
+													<input class="form-control m-input" type="number" value="" id="example-email-input">
+												</div>
+											</div>
+											<div class="form-group m-form__group row">
+												<label for="example-url-input" class="col-7 col-form-label">4. ¿Vives en casa Alquilada?</label>
+												<div class="col-5">
+													<input class="form-control m-input" type="number" value="" id="example-url-input">
+												</div>
+											</div>
+											<div class="form-group m-form__group row">
+												<label for="example-tel-input" class="col-7 col-form-label">5. ¿Por cuántos miembros esta compuesto la unidad familiar?</label>
+												<div class="col-5">
+													<input class="form-control m-input" type="number" value="" id="example-tel-input">
+												</div>
+											</div>
+											<div class="form-group m-form__group row">
+												<label for="example-password-input" class="col-7 col-form-label">7. ¿Usted cuenta con aporte propio?</label>
+												<div class="col-5">
+													<input class="form-control m-input" type="text" value="" id="example-password-input">
+												</div>
+											</div>
+											<div class="form-group m-form__group row">
+												<label for="example-number-input" class="col-7 col-form-label">8. ¿Es casado o conviviente?</label>
+												<div class="col-5">
+													<input class="form-control m-input" type="text" value="" id="example-number-input">
+												</div>
+											</div>
+											
+										</div>
+										<div class="m-portlet__foot m-portlet__foot--fit">
+											<div class="m-form__actions">
+												<div class="row">
+													<div class="col-2">
+													</div>
+													<div class="col-10">
+														<button type="reset" class="btn btn-warning">Limpiar</button>
+														<button type="submit" class="btn btn-success">Calcular</button>
+													</div>
+												</div>
+											</div>
+										</div>
+									</form>
+								</div>
+
+								<!--end::Portlet-->
+
+								<!--begin::Portlet-->
+								
+
+								<!--end::Portlet-->
+
+								<!--begin::Portlet-->
+							
+
+								<!--end::Portlet-->
+							</div>
+							<div class="col-md-6">
+
+								<!--begin::Portlet-->
+								
+
+								<!--end::Portlet-->
+
+								<!--begin::Portlet-->
+								
+
+								<!--end::Portlet-->
+
+								<!--begin::Portlet-->
+							
+
+								<!--end::Portlet-->
+
+								<!--begin::Portlet-->
+								
+
+								<!--end::Portlet-->
+
+
+								<!--end::Portlet-->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			
