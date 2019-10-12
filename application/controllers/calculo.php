f2c11ba1b4484a8729d4118f27f8ab2a9c457864
rodrigosecko <rodrigosecko@gmail.com>
2019-10-12 11:57:52 -0400

    registro de los datos para el calculo

--- application/controllers/calculo.php
+++ application/controllers/calculo.php
@@ -20,11 +20,45 @@ class Calculo extends CI_Controller {
 	 */
 	public function index()
 	{
+		redirect(base_url() . 'inicio');
+	}
+	public function nuevo($id=null)
+	{
 		$this->load->view('admin/header');
-		$this->load->view('calculo');
+		$data['beneficiario_id'] = $id;
+		$this->load->view('calculo', $data);
 		
-		// $this->load->view('admin/menu');
 		$this->load->view('admin/footer');
-       
+	}
+	public function create()
+	{
+		$datos = $this->input->post();
+		if(isset($datos))
+		{
+			$data = array(
+
+            //'codcatas' => $this->input->post('cod_catastral'), //input
+            'beneficiario_id' => $this->input->post('beneficiario_id'), //input
+            'ingreso_mensual' => $this->input->post('ingreso_mensual'), //crear
+            'ingreso_conyugue' => $this->input->post('ingreso_conyugue'),
+            'deuda' => $this->input->post('deuda'),
+            'deuda_banco' => $this->input->post('deuda_banco'),
+            'alquiler' => $this->input->post('alquiler'),
+            'pago_alquiler' => $this->input->post('pago_alquiler'),
+            'miembros' => $this->input->post('miembros'), //validar
+            'aporte' => $this->input->post('aporte'),
+            'aporte_beneficiario' => $this->input->post('aporte_beneficiario'),
+            'casado' => $this->input->post('casado'),
+            'prestamo' => 0,
+            'cuota_mensual' => 0,
+            //'activo' => '1',
+        );
+			$this->db->insert('credito', $data);
+			redirect(base_url() . 'Calculo/nuevo/' . $this->input->post('beneficiario_id'));
+
+		}
+		else {
+            redirect(base_url());
+        }
 	}
 }
--- application/views/calculo.php
+++ application/views/calculo.php
@@ -10,7 +10,7 @@
 									}
 								</style>
 								<!-- begin::Body -->
-								<div class="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body" style="background-image: url(../vivo/public/assets/imagenes/formulario2.jpg);
+								<div class="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body" style="background-image: url(<?php echo base_url(); ?>/public/assets/imagenes/formulario2.jpg);
 		  overflow: hidden;">
 
 									<!-- BEGIN: Left Aside -->
@@ -52,66 +52,68 @@
 														</div>
 
 														<!--begin::Form-->
-														<form class="m-form m-form--fit m-form--label-align-right">
+														<?php echo form_open('Calculo/create', array('method'=>'POST', 'class' => 'm-form m-form--fit m-form--label-align-right')); ?>
+														<!--<form class="m-form m-form--fit m-form--label-align-right">-->
+															<input type="hidden" value="<?php echo $beneficiario_id; ?>"name="beneficiario_id">
 															<div class="m-portlet__body">
 
 																<div class="form-group m-form__group row">
 																	<label for="example-text-input" class="col-9 col-form-label">1. ¿A cuánto alcanza tus ingresos liquidos mensuales? (Bs.)</label>
 																	<div class="col-3">
-																		<input class="form-control m-input m-input--air m-input--pill" type="number" value="" id="example-text-input">
+																		<input class="form-control m-input m-input--air m-input--pill" type="number" value="" id="example-text-input" name="ingreso_mensual" required="">
 																	</div>
 																</div>
 																<div class="form-group m-form__group row">
 																	<label for="example-search-input" class="col-9 col-form-label">2. ¿A cuanto asciende tus ingresos adicionales o de tu conyuge (si corresponde)? (Bs.)</label>
 																	<div class="col-3">
-																		<input class="form-control m-input m-input--air m-input--pill" type="number" value="How do I shoot web" id="example-search-input">
+																		<input class="form-control m-input m-input--air m-input--pill" type="number" value="How do I shoot web" id="example-search-input" name="ingreso_conyugue" required="">
 																	</div>
 																</div>
 																<div class="form-group m-form__group row">
 																	<label class="col-xl-9 col-lg-9 col-form-label">3. ¿Tienes préstamos en otras entidades financieras?</label>
 																	<div class="col-xl-3 col-lg-3">
-																		<select name="country" class="form-control m-input m-input--air m-input--pill" onchange="showDivPagoEF('hidden_pago_ef', this)">
+																		<select name="deuda" class="form-control m-input m-input--air m-input--pill" onchange="showDivPagoEF('hidden_pago_ef', this)" required="">
 																			<option value=""></option>
 																			<option value="1">SI</option>
-																			<option value="2">NO</option>
+																			<option value="0">NO</option>
 																		</select>
 																	</div>
 																</div>
 																<div class="form-group m-form__group row" 	id="hidden_pago_ef">
 																	<label for="example-email-input" class="col-9 col-form-label">3.1 ¿Cuánto pagas mensualmente por tus prestamos en otras entidades financieras?</label>
 																	<div class="col-3">
-																		<input class="form-control m-input m-input--air m-input--pill" type="number" value="" id="example-email-input">
+																		<input class="form-control m-input m-input--air m-input--pill" type="number" value="" id="example-email-input" name="deuda_banco" required="">
 																	</div>
 																</div>
 																<div class="form-group m-form__group row">
 																	<label class="col-xl-9 col-lg-9 col-form-label" >4. ¿Vives en casa Alquilada?</label>
 																	<div class="col-xl-3 col-lg-3">
-																		<select name="country" class="form-control m-input m-input--air m-input--pill" onchange="showDivPago('hidden_pago_m', this)">
+																		<select name="alquiler" class="form-control m-input m-input--air m-input--pill" onchange="showDivPago('hidden_pago_m', this)" required="">
 																			<option value=""></option>
 																			<option value="1">SI</option>
-																			<option value="2">NO</option>
+																			<option value="0">NO</option>
 																		</select>
 																	</div>
 																</div>
 																<div class="form-group m-form__group row" id="hidden_pago_m">
 																	<label for="example-url-input" class="col-9 col-form-label">4.1 ¿Cuánto pagas mensualmente de alquiler?</label>
 																	<div class="col-3">
-																		<input class="form-control m-input m-input--air m-input--pill" type="number" value="" id="example-url-input">
+																		<input class="form-control m-input m-input--air m-input--pill" type="number" value="" id="example-url-input" name="pago_alquiler" required="">
 																	</div>
 																</div>
 																<div class="form-group m-form__group row">
 																	<label for="example-tel-input" class="col-9 col-form-label">5. ¿Por cuántos miembros esta compuesto la unidad familiar?</label>
 																	<div class="col-3">
-																		<input class="form-control m-input m-input--air m-input--pill" type="number" value="" id="example-tel-input">
+																		<input class="form-control m-input m-input--air m-input--pill" type="number" value="" id="example-tel-input" name="miembros" required="">
 																	</div>
 																</div>
 																<div class="form-group m-form__group row">
 																	<label class="col-xl-9 col-lg-9 col-form-label">6. ¿Usted cuenta con aporte propio?</label>
 																	<div class="col-xl-3 col-lg-3">
-																		<select name="country" class="form-control m-input m-input--air m-input--pill" onchange="showDiv('hidden_div', this)">
+																		<select name="aporte" class="form-control m-input m-input--air m-input--pill" onchange="showDiv('hidden_div', this)" required="">
 																			<option value=""></option>
 																			<option value="1">SI</option>
-																			<option value="2">NO</option>
+																			<option value="0">NO</option>
 																		</select>
 																	</div>
 																</div>
@@ -120,24 +122,19 @@
 																<div class="form-group m-form__group row" id="hidden_div">
 																	<label for="example-password-input" class="col-9 col-form-label">6.1. ¿A cuanto alcanza su aporte propio? (AHORRO EN EFECTIVO Y/O ANTICRETICO) ($us)</label>
 																	<div class="col-3">
-																		<input class="form-control m-input m-input--air m-input--pill" type="text" value="" id="example-password-input">
+																		<input class="form-control m-input m-input--air m-input--pill" type="text" value="" id="example-password-input" name="aporte_beneficiario" required="">
 																	</div>
 																</div>
 																<div class="form-group m-form__group row">
 																	<label class="col-xl-9 col-lg-9 col-form-label">7. ¿Es casado o conviviente?</label>
 																	<div class="col-xl-3 col-lg-3">
-																		<select name="country" class="form-control m-input m-input--air m-input--pill">
+																		<select name="casado" class="form-control m-input m-input--air m-input--pill" required="">
 																			<option value=""></option>
-																			<option value="AF">SI</option>
-																			<option value="AX">NO</option>
+																			<option value="1">SI</option>
+																			<option value="0">NO</option>
 																		</select>
 																	</div>
 																</div>
-
-
-
-
-
 															</div>
 															<div class="m-portlet__foot m-portlet__foot--fit">
 																<div class="m-form__actions">
