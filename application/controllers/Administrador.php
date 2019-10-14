a6599083ad75a4ddbc307750534129114c19090c
rodrigosecko <rodrigosecko@gmail.com>
2019-10-14 10:47:00 -0400

    data tables spanish funcional y modificacion CRUD condominios

--- application/controllers/Administrador.php
+++ application/controllers/Administrador.php
@@ -20,17 +20,17 @@ class Administrador extends CI_Controller {
 	 */
 
 	public function __construct()
-    {
-        parent::__construct();
-        $this->load->model("Administrador_model");
+	{
+		parent::__construct();
+		$this->load->model("Administrador_model");
         //$this->load->library('session');
-        
-        
-        $this->load->helper('url_helper');
-        $this->load->helper('vayes_helper');
-        
-        
-    }
+
+
+		$this->load->helper('url_helper');
+		$this->load->helper('vayes_helper');
+
+
+	}
 	public function index()
 	{
 		$this->load->view('crud/header');	
@@ -47,49 +47,94 @@ class Administrador extends CI_Controller {
 	public function create()
 	{
 		$datos = $this->input->post();
+		$precio = $this->input->post('valor');
+
 		if(isset($datos))
 		{
-			$data = array(
+			$montos_cuota = $this->calcula_cuota_mes($precio, 0.0045, 300);
+			//var_dump($montos_cuota);
+			//exit;
 
-            //'codcatas' => $this->input->post('cod_catastral'), //input
+			
+				$a=$montos_cuota['cuota'];
+				$b=$montos_cuota['sueldo_ideal'];				
+		
+
+			$data = array(            
             'descripcion' => $this->input->post('descripcion'), //input
             'ciudad' => $this->input->post('ciudad'), //crear
             'valor' => $this->input->post('valor'),
+            'disponible' => $this->input->post('disponible'),
+            'superficie' => $this->input->post('superficie'),
+            'cuota_mensual' =>$a ,
+            'sueldo_prom' => $b,
         );
 			$this->db->insert('condominio', $data);
 			redirect(base_url() . 'Administrador');
-
 		}
 		else {
-            redirect(base_url());
-        }
+			redirect(base_url());
+		}
 	}
 	public function update()
 	{
 		$datos = $this->input->post();
 		if(isset($datos))
 		{
+			$precio = $this->input->post('valor_e');
+			$montos_cuota = $this->calcula_cuota_mes($precio, 0.0045, 300);
+			//var_dump($montos_cuota);
+			//exit;
+
+			
+				$a=$montos_cuota['cuota'];
+				$b=$montos_cuota['sueldo_ideal'];	
 			$id = $this->input->post('id_e'); //input
 			$data = array(            
             'descripcion' => $this->input->post('descripcion_e'), //input
             'ciudad' => $this->input->post('ciudad_e'), //crear
             'valor' => $this->input->post('valor_e'),
+            'disponible' => $this->input->post('disponible_e'),
+            'superficie' => $this->input->post('superficie_e'),
+            'cuota_mensual' =>$a ,
+            'sueldo_prom' => $b,
         );
 
-			 $this->db->where('id', $id);
-        	$this->db->update('condominio', $data);
-		
+			$this->db->where('id', $id);
+			$this->db->update('condominio', $data);
+
 			redirect(base_url() . 'Administrador');
 
 		}
 		else {
-            redirect(base_url());
-        }
+			redirect(base_url());
+		}
 	}
 
 	public function delete($id=null)
 	{
 		$this->db->delete('condominio', array('id' => $id));
 		redirect(base_url() . 'Administrador');
+	}
+	function calcula_cuota_mes($monto_prestamo = null, $porcentaje = null, $cuotas = null)
+	{
+		// $montos_cuota = $this->calcula_cuota_mes(208800, 0.0045, 300);
+		// $valor = 208800*((0,0045*(1+0,0045)^300)/((1+0,0045)^300-1))
+		$cuota_mensual = $monto_prestamo * (($porcentaje * pow((1 + $porcentaje), $cuotas)) / (pow((1 + $porcentaje), $cuotas) - 1));
+		$porcentaje_ajuste = $cuota_mensual*0.01;
+		$monto_ajustado = $cuota_mensual + $porcentaje_ajuste;
+		$monto_redondeado = round($monto_ajustado, 2);
+
+		$seguro_incendio = $monto_prestamo*0.00015;
+		$cuota_total = $monto_redondeado + $seguro_incendio;//insertar
+
+		$sueldo_ideal = round($cuota_total/0.4, 2);
+
+		$resultados['cuota'] = $monto_redondeado;
+		$resultados['seguro'] = $seguro_incendio;
+		$resultados['cuota_total'] = $cuota_total;
+		$resultados['sueldo_ideal'] = $sueldo_ideal;
+		// vdebug($sueldo_ideal, true, false, true);
+		return $resultados;
 	}
 }
--- application/views/crud/footer.php
+++ application/views/crud/footer.php
@@ -1,50 +1,112 @@
 <!-- begin::Footer -->
-			
-			<!-- end::Footer -->
-		</div>
 
-		<!-- end:: Page -->
+<!-- end::Footer -->
+</div>
+
+<!-- end:: Page -->
 
 
-		<!-- begin::Scroll Top -->
-		<div id="m_scroll_top" class="m-scroll-top">
-			<i class="la la-arrow-up"></i>
-		</div>
+<!-- begin::Scroll Top -->
+<div id="m_scroll_top" class="m-scroll-top">
+	<i class="la la-arrow-up"></i>
+</div>
 
-		<!-- end::Scroll Top -->
-
-		
-
-		<!--begin::Global Theme Bundle -->
-		<script src="<?php echo base_url(); ?>public/assets/vendors/base/vendors.bundle.js" type="text/javascript"></script>
-		<script src="<?php echo base_url(); ?>public/assets/demo/default/base/scripts.bundle.js" type="text/javascript"></script>
+<!-- end::Scroll Top -->
 
 
 
-		<script src="<?php echo base_url(); ?>public/assets/vendors/custom/datatables/datatables.bundle.js" type="text/javascript"></script>
-							<script>
-        function agregarform(data)
-        {
-             d=data.split('||');
-             $('#id_e').val(d[0]);
-              $('#descripcion_e').val(d[1]);
-              $('#ciudad_e').val(d[2]);
-              $('#valor_e').val(d[3]);
-        }
+<!--begin::Global Theme Bundle -->
+<script src="<?php echo base_url(); ?>public/assets/vendors/base/vendors.bundle.js" type="text/javascript"></script>
+<script src="<?php echo base_url(); ?>public/assets/demo/default/base/scripts.bundle.js" type="text/javascript"></script>
+
+<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.js"></script>
+
+<script>
+    $(function() {
+        $('#tabladatos').DataTable();
+        $(document).ready(function() {
+            var table = $('#example').DataTable({
+                "columnDefs": [{
+                    "visible": false,
+                    "targets": 2
+                }],
+                "order": [
+                    [2, 'asc']
+                ],
+                "displayLength": 25,
+                "drawCallback": function(settings) {
+                    var api = this.api();
+                    var rows = api.rows({
+                        page: 'current'
+                    }).nodes();
+                    var last = null;
+                    api.column(2, {
+                        page: 'current'
+                    }).data().each(function(group, i) {
+                        if (last !== group) {
+                            $(rows).eq(i).before('<tr class="group"><td colspan="5">' + group + '</td></tr>');
+                            last = group;
+                        }
+                    });
+                }
+            });
+            // Order by the grouping
+            $('#example tbody').on('click', 'tr.group', function() {
+                var currentOrder = table.order()[0];
+                if (currentOrder[0] === 2 && currentOrder[1] === 'asc') {
+                    table.order([2, 'desc']).draw();
+                } else {
+                    table.order([2, 'asc']).draw();
+                }
+            });
+        });
+    });
+    $('#example23').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+        ]
+    });
+    $('.buttons-copy, .buttons-csv, .buttons-print, .buttons-pdf, .buttons-excel').addClass('btn btn-primary mr-1');
+    $('#tabladatos').DataTable( {
+        "order": [[ 0, "desc" ]],
+        "oLanguage": {
+            "sUrl": "https://cdn.datatables.net/plug-ins/1.10.16/i18n/Spanish.json"
+        },
+    });
+
 
     </script>
 
-		<!--end::Page Vendors -->
 
-		<!--begin::Page Scripts -->
-		
-		<!--end::Global Theme Bundle -->
 
-		<!--begin::Page Vendors -->
-		
 
-		<!--end::Page Scripts -->
-	</body>
 
-	<!-- end::Body -->
+<script>
+	function agregarform(data)
+	{
+		console.log(data);
+		d=data.split('||');
+		$('#id_e').val(d[0]);
+		$('#descripcion_e').val(d[1]);
+		$('#ciudad_e').val(d[2]);
+		$('#disponible_e').val(d[3]);
+		$('#superficie_e').val(d[4]);
+		$('#valor_e').val(d[5]);
+	}
+</script>
+
+<!--end::Page Vendors -->
+
+<!--begin::Page Scripts -->
+
+<!--end::Global Theme Bundle -->
+
+<!--begin::Page Vendors -->
+
+
+<!--end::Page Scripts -->
+</body>
+
+<!-- end::Body -->
 </html>
