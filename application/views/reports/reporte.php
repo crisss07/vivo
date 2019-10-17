a5c18ea0a88d62e2e062ec35d627958282804005
rodrigosecko <rodrigosecko@gmail.com>
2019-10-17 17:25:34 -0400

    reporte

---
+++ application/views/reports/reporte.php
@@ -0,0 +1,191 @@
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Certificado de no Catastro</title>
+
+    <style type="text/css">
+        @page {
+
+            margin: 0px;
+        }
+
+        body {
+
+
+            background-image: url('<?php echo base_url(); ?>public/assets/reportes/menbrete.png');
+
+            background-repeat: no-repeat; 
+
+
+        }
+
+        * {
+            font-family: Verdana, Arial, sans-serif;
+        }
+
+        a {
+            color: #fff;
+            text-decoration: none;
+        }
+
+        table {
+            font-size: 13px;
+            line-height:14px;
+            
+            
+        }
+
+
+
+        tfoot tr td {
+            font-weight: bold;
+            font-size: x-small;
+        }
+
+        .invoice {
+            padding:90px;
+        }
+
+
+    
+
+     
+        .tit1{
+            font-size: 14px;
+            line-height:26px;
+            font-style: italic; 
+            
+        }
+         .tit2{
+            font-size: 24px;
+            line-height:26px;
+            font-style: italic;
+            font-weight:bold;
+            color:#377091;
+            
+        }
+
+
+    </style>
+
+
+</head>
+<body>
+
+    <div >
+        <br><br><br>
+    </div>
+
+
+
+
+
+
+
+    <div class="invoice">
+        <h3 align="center" >SOLICITUD DE INICIO DE TRAMITE</h3>
+
+        <hr>
+        <p class="tit2" align="center">Numero de Tramite: 0001</p>
+        <hr>
+
+        <p></p><br>
+
+        <table width="60%">
+            <tr>
+                <td class="tit1" style="background-color: #ebf3f3;border-collapse: collapse;padding: 2px;">            
+                   <p></p>
+                   DATOS DEL SOLICITANTE:
+               </td> 
+               <td class="tit1" style="background-color: #ebf3f3;border-collapse: collapse;padding: 2px;">
+                
+            </td>
+        </tr>
+        <tr>
+            <td class="tit1">
+                Nombre: 
+            </td>                    
+            <td class="tit1">
+                CRISTIAN RODRIGO CHAMBY SALINAS
+
+            </td>
+        </tr>
+        <tr style="background-color: #ebf3f3;border-collapse: collapse;padding: 2px;">
+            <td class="tit1">
+                Carnet de Identidad:
+            </td>
+            <td class="tit1">
+                9112739
+            </td>
+        </tr>
+        <tr >
+            <td class="tit1">
+                 Fecha de Nacimiento
+            </td>
+            <td class="tit1">
+                1992-10-20
+            </td>
+        </tr>
+        <tr style="background-color: #ebf3f3;border-collapse: collapse;padding: 2px;">
+            <td class="tit1">
+               Numero de Celular:
+            </td>
+            <td class="tit1">
+                69824799
+            </td>
+        </tr>
+        <tr>
+            <td class="tit1">
+                Domicilio
+            </td>
+            <td class="tit1">
+                Av. McaI. Santa Cruz - Edif. La Primera - P.8 Bloque A
+            </td>
+        </tr>
+        <tr style="background-color: #ebf3f3;border-collapse: collapse;padding: 2px;">
+            <td class="tit1">
+                Correo Electronico
+            </td>
+            <td class="tit1">
+                rodrigo@gmail.com
+            </td>
+        </tr>
+        <tr>
+            <td class="tit1">
+                Departamento
+            </td>
+            <td class="tit1">
+                Cochabamba
+
+            </td>
+        </tr>
+        <tr style="background-color: #ebf3f3;border-collapse: collapse;padding: 2px;">
+            <td class="tit1">
+                
+            </td>
+            <td class="tit1">
+
+            </td>
+        </tr>
+        <tr>
+            <td class="tit1">
+
+            </td>
+            <td class="tit1">
+
+            </td>
+        </tr>
+
+
+    </table>
+
+
+
+
+
+
+</div>
+
+</body>
+</html>
