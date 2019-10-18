669d6de671dc8bb90c721006226a44d997e7aaa9
rodrigosecko <rodrigosecko@gmail.com>
2019-10-18 11:26:09 -0400

    video

---
+++ application/views/splash.php
@@ -0,0 +1,51 @@
+<!-- begin::Body -->
+
+<iframe width="100%" height="90%" src="https://www.youtube.com/embed/pSO9sjenRHQ?autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+			<div class="m-grid__item m-grid__item--fluid m-grid m-grid--ver-desktop m-grid--desktop m-body" style="background-image: url(<?php echo base_url(); ?>/public/assets/imagenes/formulario1.jpg);
+		  overflow: hidden;">
+
+				<!-- BEGIN: Left Aside -->
+				
+
+					<!-- BEGIN: Aside Menu -->
+					
+
+					<!-- END: Aside Menu -->
+
+				<!-- END: Left Aside -->
+				<div class="m-grid__item m-grid__item--fluid m-wrapper">
+					
+					<div class="d-flex align-items-center">
+						<div class="mr-auto">
+							<h3 class="m-subheader__title "> </h3>
+						</div>
+						<!-- <div  style=" float: right; margin-right: 20px; ">
+							<span class="m-subheader__daterange" id="m_dashboard_daterangepicker">
+								<a href="<?php //base_url('Administrador'); ?>" type="button" class="btn m-btn--pill btn-success">Administrar</a>
+								
+							</span>
+						</div> -->
+					</div>
+
+					<!-- BEGIN: Subheader -->
+					
+
+					<!-- END: Subheader -->
+					<div class="m-content">
+						<div class="row">
+								<div class="col-md-12" align="center">									
+									<a href="<?php echo base_url(); ?>/Inicio" type="button" class="btn m-btn--pill btn-success">Empezar</a>								  
+								</div>					
+							
+						</div>
+					</div>
+
+				
+				</div>
+
+			</div>
+
+			<!-- end:: Body -->
+
+
